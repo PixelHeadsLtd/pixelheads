b0446acaa545786042df05558ecec106c5e04635
Mike King <info@pixel-heads.com>
2019-09-25 21:01:06 +0100

    some animation

--- src/components/pages/landing-page.js
+++ src/components/pages/landing-page.js
@@ -3,11 +3,17 @@ import React, { Component } from 'react';
 import LinkedIn from '../../images/social/linkedin.svg';
 import FB from '../../images/social/fb.svg';
 import GitHub from '../../images/social/github.svg';
+import Mike from '../../images/mike.png';
+//import Waves from '../../images/sand.mp4';
 
 class Landing extends Component {
   render() {
     return (
-      <div className="home-wrapper">
+      <div>
+        <img className="mike" src={Mike} alt="" />
+        {/* <video className='waves' autoPlay loop muted>
+          <source src={Waves} type='video/mp4' />
+        </video> */}
         <h1>Hi<span className="ellipsis">...</span></h1>
         <p>I’m <a href="mailto:info@pixel-heads.com">Mike King</a> of PixelHeads Limited<br />
         A Senior UX/UI Consultant, Designer, Artist, Sculptor & Musician.<br />
